bed9efbbf112bac16f2d43e026c5c7bc5cf92e1a
foxpwnzz <s.a.golubev90@gmail.com>
2021-10-11 23:34:33 +0300

    lesson 7

--- homework_01/main.py
+++ homework_01/main.py
@@ -13,7 +13,7 @@
 def power_numbers(list):
     power_numbers_list = []
     for i in list:
-        power_numbers_list.append(i**2)
+        power_numbers_list.append(i * i)
     return power_numbers_list
 print(power_numbers([1, 2, 5, 7]))
 
@@ -23,30 +23,18 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
+ODD = [1, 2, 3, 4, 5, 6, 7, 8]
+for num in ODD:
+    if num % 2 == 0:
+         print(num)
 
-def filter_numbers():
+print('==================')
 
-    """
-функция, которая на вход принимает список из целых чисел,
-и возвращает только чётные/нечётные/простые числа
-(выбор производится передачей дополнительного аргумента)
+EVEN = [1, 2, 3, 4, 5, 6, 7, 8]
+for num in EVEN:
+    if num % 2 != 0:
+         print(num)
 
->>> filter_numbers([1, 2, 3], ODD)
-<<< [1, 3]
->>> filter_numbers([2, 3, 4, 5], EVEN)
-<<< [2, 4]
-    """
 
-def find_odds(numbers, odds):
-    if len(numbers) == 0:
-        return
-    v = numbers.pop()
-    if v % 2 == 1:
-        odds.append(v)
-    find_odds(numbers, odds)
 
-odds = []
-numbers = [1,2,3,4,5,6,7]
-find_odds(numbers,odds)
-# Now odds has the odd numbers
-print(odds)
+
