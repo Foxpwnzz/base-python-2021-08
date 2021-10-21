0a0e44c39db97a27c72869480ada3789bb72eb85
foxpwnzz <s.a.golubev90@gmail.com>
2021-10-21 23:14:18 +0300

    mmm

--- homework_01/main.py
+++ homework_01/main.py
@@ -3,38 +3,42 @@
 Функции и структуры данных
 """
 
-"""
-функция, которая принимает N целых чисел,
-и возвращает список квадратов этих чисел
->>> power_numbers(1, 2, 5, 7)
-<<< [1, 4, 25, 49]
-"""
+def power_numbers(*args):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    return [x * x for x in args]
 
-def power_numbers(list):
-    power_numbers_list = []
-    for i in list:
-        power_numbers_list.append(i * i)
-    return power_numbers_list
-print(power_numbers([1, 2, 5, 7]))
-
-
-# filter types
+#  filter types
 ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
+out_ls = []
+def prime_numbers(args):
+        k = 0
+        for n in range(1, args + 1):
+            if args % n == 0:
+                k += 1
+        if k == 2:
+            return args
 
-ODD = [1, 2, 3, 4, 5, 6, 7, 8]
-for num in ODD:
-    if num % 2 == 0:
-         print(num)
-
-print('==================')
-
-EVEN = [1, 2, 3, 4, 5, 6, 7, 8]
-for num in EVEN:
-    if num % 2 != 0:
-         print(num)
-
-
-
-
+def filter_numbers(ls, args):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    if args == ODD:
+        return [x for x in ls if x % 2 != 0]
+    elif args == EVEN:
+        return [x for x in ls if x % 2 == 0]
+    elif args == PRIME:
+        ls_prime = list(filter(prime_numbers, ls))
+        return ls_prime
