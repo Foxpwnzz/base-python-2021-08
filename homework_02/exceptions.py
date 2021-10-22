2e886711d590791e2f8de204d6ff308f1e019862
foxpwnzz <s.a.golubev90@gmail.com>
2021-10-22 20:22:20 +0300

    2 homework OTUS python Developer. Basic

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -4,12 +4,3 @@
 - NotEnoughFuel
 - CargoOverload
 """
-class LowFuelError(Exception):
-    def __str__(self):
-        return "LowFuelError"
-class NotEnoughFuel(Exception):
-    def __str__(self):
-        return "NotEnoughFuel"
-class CargoOverload(Exception):
-    def __str__(self):
-        return "CargoOverload"
